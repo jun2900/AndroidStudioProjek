10d6a37926ed75b167b8ab1d04deee9f13792bdc
jun2900 <jeremiahmaramis00@gmail.com>
2019-05-07 15:10:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tebaklagu/JenisPermainan.java
@@ -0,0 +1,27 @@
+package com.example.tebaklagu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class JenisPermainan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jenis_permainan);
+    }
+    private void Btn1(){
+        Button next = (Button) findViewById(R.id.tebakLagu);
+        System.out.println(next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(JenisPermainan.this, TebakLagu.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/tebaklagu/TebakLagu.java
@@ -0,0 +1,13 @@
+package com.example.tebaklagu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TebakLagu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tebak_lagu);
+    }
+}
